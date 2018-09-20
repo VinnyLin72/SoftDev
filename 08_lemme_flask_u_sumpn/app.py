4490b72842a7d7b762e1c510b7ffc406c85f992d
Vincent Lin <vlin6@stuy.edu>
2018-09-19 23:25:43 -0400

    finished hw

---
+++ 08_lemme_flask_u_sumpn/app.py
@@ -0,0 +1,23 @@
+#Vincent Lin
+#SoftDev1 pd7
+#K08 -- Fill Yer Flask
+#2018--09--19
+
+from flask import Flask
+App = Flask(__name__)
+
+@app.route("/route")
+def route1():
+	return "route number A"
+
+@app.route("/route")
+def route2():
+	return "route letter 2"
+
+@app.route("/route")
+def route3():
+	return "3st route"
+
+if __name__ = "__main__":
+	app.denug = True
+	app.run()
