a4211a7bc300d0b1b18d49244828d19c4c9720f9
Vincent Lin <vlin6@stuy.edu>
2018-09-24 22:01:35 -0400

    commented

--- 10_occupy_flask_st/occupation.py
+++ 10_occupy_flask_st/occupation.py
@@ -4,38 +4,38 @@
 #2018 -- 09 -- 24
 
 from flask import Flask, render_template
-import csv
-from random import choice
+import csv 
+from random import choice #importing all necessary functions
 
 app = Flask(__name__)
 
+joblist = {} #creates dictionary of jobs and percentages
+header = [] #the first line of the csv file and the table heading
+
 reader = csv.reader(open('occupations.csv', 'r'))
-joblist = {}
-header = []
 
 r = 0
-
-for row in reader:
+for row in reader: #reads every line except the first(header) in the csv file and adds the job/percentage to the dict
     if r != 0:
         joblist[row[0]] = row[1]
     else:
-        header = row
+        header = row #adds the header to the header list
     r += 1
-#print(joblist.keys())
-keys = []
-for key in joblist.keys():
-    keys.append(key)
 
-random = choice(keys[0:-1])
+def getRand(): #randomly choose a job
+    keys = []
+    for key in joblist.keys():
+        keys.append(key) #create a list of keys(jobs)
+    return choice(keys[0:-1]) #choose a random job from list
 
 
 @app.route("/")
-def home():
-	return "<a href = occupations>Occupations</a>"
+def home(): #creates an unnecessary home page with a link to assignment
+	return "<a href = occupations>Click here for occupations</a>"
 
-@app.route("/occupations")
-def occupations():
-    return render_template('occupations.html', random = random, jobs = joblist, header = header)
+@app.route("/occupations") 
+def occupations(): #uses template to create the table
+    return render_template('occupations.html', random = getRand(), jobs = joblist, header = header)
 
 if __name__ == "__main__":
 	app.debug = True
