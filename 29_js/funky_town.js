3eaa12eb628dca374c40ed2b4768d532af548667
Vincent Lin <vlin6@stuy.edu>
2018-12-19 21:32:28 -0500

    added working version of K29

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -1,6 +1,6 @@
-// Team WestVirginia - Jared Asch, Vincent Lin
+// Team Undecided - Vincent Lin, Mai Rachlevsky
 // SoftDev1 pd07
-// K28 -- Sequential Programming
+// K29 -- Sequential Progression II: Electric Boogaloo
 // 2018-12-19
 
 var fibonacci = function(n){
@@ -11,19 +11,46 @@ var fibonacci = function(n){
     }
 }
 
-let gcd = (a, b) => {
-    if(b == 0){
+/* greatest common demoniator, Euclidean algorithm??? */
+var gcd = function(a,b){
+    if (b!=0)
+	return gcd(b, a%b);
+    else{
 	return a;
     }
-    return gcd(b, a % b);
-}
+};
 
-let students = ["Vincent Lin", "Jared Asch", "Shin Bamba", "Timothy 'Timothy MC Gaming' Marder", "Teddy Peters"]
+/* define a list for convenience */
+var students = ['ananke', 'boule', 'irene', 'metron', 'ho polemos', 'philos','pratto', 'pisteuo', 'erchomai'];
 
-let random_student = (list) => {
-    return list[ Math.floor(Math.random() * list.length) ];
-}
-
+/* input list, returns a random element */
+var randomStudent = function(list){
+    index = parseInt(Math.random()*list.length);
+    return list[index];
+};
 
 var fibbutton = document.getElementById("fib");
-fibbutton.addEventListener('click', fibonacci(8));
+
+var getFib = function() {
+	console.log(fibonacci(4));
+	document.getElementById("fib")
+}
+fibbutton.addEventListener('click', getFib);
+
+    
+var gcdbutton = document.getElementById("gcd");
+
+var getGcd = function() {
+    console.log(gcd(20,50));
+    document.getElementById("gcd")
+}
+gcdbutton.addEventListener('click', getGcd);
+
+
+var randbutton = document.getElementById("student");
+
+var getStudent = function() {
+    console.log(randomStudent(students));
+    document.getElementById("student")
+}
+randbutton.addEventListener('click', getStudent);
